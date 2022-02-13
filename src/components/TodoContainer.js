85e3eb88af3285c52c41893680e7af77c89f1404
Joboson Solomon Chiakonam <solobachi02@gmail.com>
2022-02-13 23:16:56 +0100

    React Todo-App First Commit

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Header from './Header'
+import TodosList from './TodosList';
+class TodoContainer extends React.Component{
+    state = {
+        todos:[
+            {
+                id: 1,
+                title: 'Set up the development environment',
+                completed: true,
+            },
+            {
+                id: 2,
+                title: 'Develop website and dd content',
+                completed: false,
+            },
+            {
+                id: 3,
+                title: 'Deploy to live server',
+                completed: false,
+            }
+        ]
+    }
+
+   handleChange = id =>{
+        this.setState({
+            todos: this.state.todos.map(todo => {
+                if(todo.id === id){
+                    todo.completed = !todo.completed;
+                }
+                return todo;
+            })
+        });
+    }
+
+render(){
+    return(
+        <div>
+            <Header/>
+           <TodosList todos ={this.state.todos} handleChangeProps ={this.handleChange}/>
+        </div>
+    );
+}
+}
+export default TodoContainer;
